186b0154125cd259cddd4833070ac53397c68c79
Mario Jerez <jerezmario1@gmail.com>
2024-12-05 01:31:29 -0600

    Issue #6: Restricted the agents' movement to be within the boundaries of the environment

--- mpe/RL_switch_sides.py
+++ mpe/RL_switch_sides.py
@@ -1,12 +1,28 @@
 import switch_sides_v0
+import numpy as np
 
+masks = {}
+buffer = 20 #cm
 env = switch_sides_v0.parallel_env(render_mode="human")
 observations, infos = env.reset()
 env.render()
 
 while env.agents:
-    # this is where you would insert your policy
-    actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+    # insert policy here
+
+    # Prevent agents from leaving environment boundaries
+    masks = {}
+    for agent in observations:
+        # Agent observations: `[self_vel, self_pos, landmark_rel_positions, other_agent_rel_positions, communication]`
+        x_pos, y_pos = observations[agent][2], observations[agent][3]
+        masks[agent] = np.array([1,
+                       1 if x_pos > buffer else 0,
+                       1 if x_pos < env.aec_env.length - buffer else 0,
+                       1 if y_pos < env.aec_env.width - buffer else 0,
+                       1 if y_pos > 0 + buffer else 0], dtype=np.int8)
+    
+    # Agent action space: `[no_action, move_left, move_right, move_down, move_up]`
+    actions = {agent: env.action_space(agent).sample(mask=masks[agent]) for agent in env.agents}
 
     observations, rewards, terminations, truncations, infos = env.step(actions)
 env.close()
