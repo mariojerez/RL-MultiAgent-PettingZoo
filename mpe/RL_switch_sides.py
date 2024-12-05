0d0ee90c2832b130629605b6c5077df83d36a98b
Mario Jerez <jerezmario1@gmail.com>
2024-12-05 12:46:22 -0600

    fixed logic isue with y-direction action masks

--- mpe/RL_switch_sides.py
+++ mpe/RL_switch_sides.py
@@ -14,11 +14,11 @@ while env.agents:
     for agent in observations:
         # Agent observations: `[self_vel, self_pos, landmark_rel_positions, other_agent_rel_positions, communication]`
         x_pos, y_pos = observations[agent][2], observations[agent][3]
-        masks[agent] = np.array([1,
-                       1 if x_pos > buffer else 0,
-                       1 if x_pos < env.aec_env.length - buffer else 0,
-                       1 if y_pos < env.aec_env.width - buffer else 0,
-                       1 if y_pos > 0 + buffer else 0], dtype=np.int8)
+        masks[agent] = np.array([1, # no action
+                       1 if x_pos > buffer else 0, # move left
+                       1 if x_pos < env.aec_env.length - buffer else 0, #move right
+                       1 if y_pos > buffer else 0, # move down
+                       1 if y_pos < env.aec_env.width - buffer else 0], dtype=np.int8) # move up
     
     # Agent action space: `[no_action, move_left, move_right, move_down, move_up]`
     actions = {agent: env.action_space(agent).sample(mask=masks[agent]) for agent in env.agents}
